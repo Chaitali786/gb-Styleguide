75672bd3f92ea31089a78b02de8fb174d43accb4
rambbusha <chaitalideore@gmail.com>
2018-11-07 14:17:49 +0100

    worked on textbox,paragraph , slidebar(pending)

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,10 +1,16 @@
-    
-            var slider = document.getElementById("myRange");
-            var output = document.getElementById("demo");
-            output.innerHTML = slider.value;
 
-            slider.oninput = function() {
-                output.innerHTML = this.value;
-            }
+
+
+function onRangeChange(){
+    
+    var slider = document.getElementById("myRange");
+    var value_temp = (slider.value - slider.getAttribute("min")) / (slider.getAttribute("max"))-slider.getAttribute("min"));
+    slider.style.backgroundImage ='-webkit-gradient(liner , left top ,right top,'+'color-stop('+value_temp+',#047a9c),'+'color-stop('+value_temp+',#c7c7c7)'+')';
+    
+}
+
+
+
+           
         
     
